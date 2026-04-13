95df13526b1deb39c226770ff008d5ecfa6aa9c9
s4mmich <sila.schirrmacher@gmail.com>
2026-04-13 22:35:12 +0200

    added database pool

--- backend/src/Persistence/DatabaseConnection.py
+++ backend/src/Persistence/DatabaseConnection.py
@@ -2,13 +2,27 @@ import os
 import oracledb
 
 
-class DatabseConnection:
+class DatabaseConnection:
+    _pool = None
     Connection: oracledb.Connection
 
     def __init__(self):
         user_password = os.environ.get("DATABASS_PASSWORD")
-        databse_user = os.environ.get("DATABASE_USER")
+        database_user = os.environ.get("DATABASE_USER")
         database_dsn = os.environ.get("DATABASE_DSN")
-        self.Connection = oracledb.connect(user=databse_user,
-                                           password=user_password,
-                                           dsn=database_dsn)
+        if DatabaseConnection._pool is None:
+            DatabaseConnection._pool = oracledb.create_pool(
+                user=database_user,
+                password=user_password,
+                dsn=database_dsn,
+                min=1,
+                max=5,
+                increment=1
+            )
+
+    @property
+    def connection(self):
+        return DatabaseConnection._pool.acquire()
+
+    def release(self, conn):
+        DatabaseConnection._pool.release(conn)
